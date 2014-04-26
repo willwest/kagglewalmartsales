f06691fd3a67877027672be7212cdd449076b07d
Will West <wmw71190@gmail.com>
2014-04-26 11:59:13 -0400

    Added analysis files, reorganized directories

---
+++ analysis/analyze.r
@@ -0,0 +1,13 @@
+require(ggplot2)
+
+theme_set(theme_gray(base_size = 18))
+
+df <- read.csv('../../data/train.w.features.csv', header=TRUE, stringsAsFactors=TRUE)
+
+store1 <- subset(df, Store==1)
+store1d1 <- subset(store1, Dept==1)
+store1d1$Date <- as.Date(store1d1$Date)
+
+
+plot <- ggplot(store1d1, aes(x=Date, y=Weekly_Sales, fill=IsHoliday)) + geom_bar(stat="identity") + scale_fill_manual(values=c("#999999", "#E69F00"))
+ggsave("../../out/weekly_sales.pdf", width=12)
