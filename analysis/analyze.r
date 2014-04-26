bda1ed0aecb844f9f9ca724faaa30ed7fbd3d96a
Will West <wmw71190@gmail.com>
2014-04-26 12:01:24 -0400

    Added history dir

--- analysis/analyze.r
+++ analysis/analyze.r
@@ -4,6 +4,12 @@ theme_set(theme_gray(base_size = 18))
 
 df <- read.csv('../../data/train.w.features.csv', header=TRUE, stringsAsFactors=TRUE)
 
+##########################################
+####### Data Exploration: Holidays #######
+# Pick out a specific store, and display the weekly sales
+# orered by date, highlighting the weeks in which there is
+# a holiday.
+
 store1 <- subset(df, Store==1)
 store1d1 <- subset(store1, Dept==1)
 store1d1$Date <- as.Date(store1d1$Date)
@@ -11,3 +17,6 @@ store1d1$Date <- as.Date(store1d1$Date)
 
 plot <- ggplot(store1d1, aes(x=Date, y=Weekly_Sales, fill=IsHoliday)) + geom_bar(stat="identity") + scale_fill_manual(values=c("#999999", "#E69F00"))
 ggsave("../../out/weekly_sales.pdf", width=12)
+
+##########################################
+##########################################
