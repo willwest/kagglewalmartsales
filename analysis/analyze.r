18d536166c9177261151d8284b9d165b27873bc8
Will West <wmw71190@gmail.com>
2014-04-30 10:33:21 -0400

    Edited report. Added scatterplot matrix.

--- analysis/analyze.r
+++ analysis/analyze.r
@@ -16,7 +16,22 @@ store1d1$Date <- as.Date(store1d1$Date)
 
 
 plot <- ggplot(store1d1, aes(x=Date, y=Weekly_Sales, fill=IsHoliday)) + geom_bar(stat="identity") + scale_fill_manual(values=c("#999999", "#E69F00"))
-ggsave("../../out/weekly_sales.pdf", width=12)
+ggsave(plot=plot, filename="../out/weekly_sales.pdf", width=12)
+
+
+####### Data Exploration: Features #######
+# Pick out a specific store, and display the weekly sales
+# orered by date, highlighting the weeks in which there is
+# a holiday.
+require(GGally)
+
+# get random sample of df rows
+df.sample <- df[sample(nrow(df), 1000), ]
+
+
+pdf(file='../out/features_scatter.pdf')
+ggpairs(df.sample[,c("Temperature", "Fuel_Price", "CPI", "Unemployment", "Size")], axisLabels="none", diag=list(continuous="density", discrete="bar"),  params=list(labelSize=3, size=1, gridLabelSize=10), upper=list(params=list(size=5)))
+
 
 ##########################################
 ##########################################
